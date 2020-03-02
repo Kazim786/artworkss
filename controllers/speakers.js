c309de1e78354177ca719c6a54ff2592f0dce358
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-03-02 16:24:29 -0600

    i did it

--- controllers/speakers.js
+++ controllers/speakers.js
@@ -4,24 +4,64 @@ let data = require('../data/data.json');
 
 
 //this one gets all the speakers 
+
+
 router.get('/speakers', (req, res)=>{
-    res.render('speakers')
     let pagePhotos = [];
     data.speakers.forEach((speakerObj)=>{
         pagePhotos = pagePhotos.concat(speakerObj.artwork)
     })
-
-})
-
-router.get('/speakers/:speakerid', (req,res) => {
     res.render('speakers', {
         pageTitle: "Speaker List",
         artistName: "",
         artwork: pagePhotos,
         allSpeakers: data.speakers,
         pageID: 'speakerList'
+
+
+    })
+   
+})
+
+router.get('/speakers/:speakerid', (req,res) => {
+    pagePhotos = [];
+    pageSpeakers = [];
+    data.speakers.forEach((speakerObj)=>{
+        if (speakerObj.shortname == req.params.speakerid){
+            pageSpeakers.push(speakerObj);
+            pagePhotos = speakerObj.artwork;
+        }
+
+
+    })
+    res.render('speakers', {
+        pageTitle: "Speaker List",
+        artistName: pageSpeakers[0].artistName,
+        artwork: pagePhotos,
+        allSpeakers: pageSpeakers,
+        pageID: 'speakerList'
     })
 })
+
+// router.get('/speaker/:speakerid', (req, res) => {
+//     let pagePhotos = [];
+//     let pageSpeakers = [];
+//     data.speakers.forEach((speakerObj)=>{
+//         if(speakerObj.shortname == req.params.speakerid){
+//             pageSpeakers.push(speakerObj);
+//             pagePhotos = speakerObj.artwork
+
+
+//         }
+//     })
+//     res.render('speakers', {
+//         pageTitle: "Speaker Details",
+//         artistName: pageSpeakers[0].artistName,
+//         artwork: pagePhotos,
+//         allSpeakers: pageSpeakers,
+//         pageID: 'pageSpeakers'
+//     } )
+// })
 
 
 module.exports = router;
